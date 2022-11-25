30563234658f87917c48c70f2a691e124d7fa2db
Rahulraj00 <52102751+Rahulraj00@users.noreply.github.com>
2022-11-25 11:37:40 +0530

    Update main.cpp

--- ota2/src/main.cpp
+++ ota2/src/main.cpp
@@ -1,9 +1,22 @@
 // #include < Wire .h> we are removing this because it is already added in liquid crystal library
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <LiquidCrystal_I2C.h>
+//    Can be client or even host   //
+#ifndef STASSID
+#define STASSID "Deadpool2"  // Replace with your network credentials
+#define STAPSK  "12345qwet"  
+#endif
+
+
  
 // Create the lcd object address 0x3F and 16 columns x 2 rows 
 LiquidCrystal_I2C lcd (0x27, 16,2);  //
  
+ const char* ssid = STASSID;
+const char* password = STAPSK;
+
 void  setup () {
    // Initialize the LCD connected 
   lcd. init ();
